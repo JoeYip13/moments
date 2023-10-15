a190e9a4a736549107da885e7023a1dabb08ace7
JoeYip13 <>
2023-10-15 08:40:59 +0000

    Adjusting NavBar for logged in / logged out

---
+++ src/components/Avatar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "../styles/Avatar.module.css";
+
+// const Avatar = (props) => {
+//     const { src, height = 45, text } = props;
+//     return <div>Avatar</div>;
+// };
+
+const Avatar = ({ src, height = 45, text }) => {
+    return (
+        <span>
+            <img
+                className={styles.Avatar}
+                src={src}
+                height={height}
+                width={height}
+                alt="avatar"
+            />
+            {text}
+        </span>
+    );
+};
+
+export default Avatar;
