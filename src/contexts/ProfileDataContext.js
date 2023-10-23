f539ef49d6761572df2d6f05e1cdc1fc148eb9f5
JoeYip13 <>
2023-10-23 17:42:03 +0000

    Add follow function for profiles

--- src/contexts/ProfileDataContext.js
+++ src/contexts/ProfileDataContext.js
@@ -1,7 +1,7 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { axiosReq } from "../api/axiosDefaults";
+import { followHelper } from "../utils/utils";
 import { useCurrentUser } from "./CurrentUserContext";
-
 
 export const ProfileDataContext = createContext();
 export const SetProfileDataContext = createContext();
@@ -17,6 +17,31 @@ export const ProfileDataProvider = ({ children }) => {
     });
 
     const currentUser = useCurrentUser();
+
+    const handleFollow = async (clickedProfile) => {
+        try {
+            const { data } = await axiosReq.post("/followers/", {
+                followed: clickedProfile.id,
+            });
+
+            setProfileData((prevState) => ({
+                ...prevState,
+                pageProfile: {
+                    results: prevState.pageProfile.results.map((profile) =>
+                        followHelper(profile, clickedProfile, data.id)
+                    ),
+                },
+                popularProfiles: {
+                    ...prevState.popularProfiles,
+                    results: prevState.popularProfiles.results.map((profile) =>
+                        followHelper(profile, clickedProfile, data.id)
+                    ),
+                },
+            }));
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
     useEffect(() => {
         const handleMount = async () => {
@@ -37,7 +62,9 @@ export const ProfileDataProvider = ({ children }) => {
 
     return (
         <ProfileDataContext.Provider value={profileData}>
-            <SetProfileDataContext.Provider value={setProfileData}>
+            <SetProfileDataContext.Provider
+                value={{ setProfileData, handleFollow }}
+            >
                 {children}
             </SetProfileDataContext.Provider>
         </ProfileDataContext.Provider>
